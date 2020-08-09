54e55ee80591afe772c6beb8b7cbba0058eb6961
jeyotha <jhon.bermudez@jeyotha.com>
2020-08-09 12:45:48 -0500

    finished create carousel, debemos apredel mas sobre el 100% del deiv

--- js/index.js
+++ js/index.js
@@ -7,13 +7,31 @@ inconMenu.addEventListener('click', () => {
 
 window.addEventListener('load', function(){
     new Glider(document.querySelector('.carousel__lista'),{
-        slidesToShow: 3,
-		slidesToScroll: 3,
+        slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: true,
 		dots: '.carousel__indicadores',
 		arrows: {
 			prev: '.carousel__anterior',
 			next: '.carousel__siguiente'
-		}
+		},
+		responsive:[
+			{
+				breakpoint:790,/*>= 910 px*/
+				settings:{
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					draggable: false
+				}
+			},{
+				breakpoint:1100,
+				settings:{
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					draggable: false
+				}
+			}
+		]
     });
 });
 /*Hola Jhon lo edito, mandelo a comer mierda*/
